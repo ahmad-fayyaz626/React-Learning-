05572c9fab634d7d3dfb06c1af9f9ba78bad78d1
ahmad-fayyaz-626 <af9387114@gmail.com>
2026-06-09 02:51:11 +0500

    Modified Folders

--- Day7/Quiz App/src/App.jsx
+++ Day7/Quiz App/src/App.jsx
@@ -5,18 +5,23 @@ import Question from "./Components/Question";
 import questions from "./Components/questions.json";
 import Result from "./Components/Result";
 const App = () => {
-  const [isEnded , setEnded] = useState(false);
+  const [isEnded, setEnded] = useState(false);
   const [score, setScore] = useState(0);
 
-  console.log (score);
+  console.log(score);
   const [showResult, setShowResult] = useState(false);
   return (
     <div className="w-screen h-screen bg-gray-200 flex flex-col justify-center items-center">
       <Timer setShowResult={setShowResult} isEnded={isEnded} />
       {!showResult ? (
-        <Question setEnded={setEnded} End={showResult} setScore={setScore} setShowResult={setShowResult} />
+        <Question
+          setEnded={setEnded}
+          End={showResult}
+          setScore={setScore}
+          setShowResult={setShowResult}
+        />
       ) : (
-        <Result score={score}  totalQuestions={questions.length} />
+        <Result score={score} totalQuestions={questions.length} />
       )}
     </div>
   );
